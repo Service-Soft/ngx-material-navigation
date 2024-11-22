f72c0759df5aac5d3805a47d57ed3a8108bdaa41
Tim Fabian <fabian@service-soft.de>
2024-11-22 02:24:15 +0100

    fixed package jsons

--- projects/ngx-material-navigation-showcase/src/main.ts
+++ projects/ngx-material-navigation-showcase/src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { InMemoryScrollingOptions, RouterConfigOptions, provideRouter, withInMemoryScrolling, withRouterConfig } from '@angular/router';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { NGX_BURGER_MENU_ARIA_LABEL, NGX_BURGER_MENU_ICON } from 'ngx-material-navigation';
 
 import { AppComponent } from './app/app.component';
@@ -29,7 +30,7 @@ bootstrapApplication(
             provideAnimations(),
             {
                 provide: NGX_BURGER_MENU_ICON,
-                useValue: 'fas fa-home'
+                useValue: faHome
             },
             {
                 provide: NGX_BURGER_MENU_ARIA_LABEL,
